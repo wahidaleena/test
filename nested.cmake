1222348940e84bbad7523e2381f4d6e4b0331aa0
wahidaleena <34679809+wahidaleena@users.noreply.github.com>
2019-05-22 09:58:05 +0530

    Update and rename nested.cmake to test.cmake

--- nested.cmake
+++
@@ -1,6 +0,0 @@
-cmake_minimum_required(VERSION 2.6)
-	add_executable(	arrays arrays.c	)
-	add_executable(	calculator calculator.c	)
-	add_executable(	convert convert.c	)
-	add_executable(	word_count word_count.c	)
-	
